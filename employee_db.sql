8cca11a4b60695ba42056e2189afda6e6b66569c
Raghib Mahatab <98721756+Starpest@users.noreply.github.com>
2023-07-26 19:53:45 -0400

    Update employee_db.sql

--- employee_db.sql
+++ employee_db.sql
@@ -1,21 +1,36 @@
 -- Create a table to store information about employees
-CREATE TABLE Employees (
-    EmployeeID INT PRIMARY KEY,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Age INT,
-    Department VARCHAR(50)
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    department VARCHAR(50),
+    salary DECIMAL(10, 2)
 );
 
--- Insert some data into the Employees table
-INSERT INTO Employees (EmployeeID, FirstName, LastName, Age, Department)
-VALUES (1, 'John', 'Doe', 30, 'Engineering');
+-- Insert some data into the employees table
+INSERT INTO employees (employee_id, first_name, last_name, age, department, salary)
+VALUES (1, 'John', 'Doe', 30, 'Engineering', 60000.00);
 
-INSERT INTO Employees (EmployeeID, FirstName, LastName, Age, Department)
-VALUES (2, 'Jane', 'Smith', 28, 'Marketing');
+INSERT INTO employees (employee_id, first_name, last_name, age, department, salary)
+VALUES (2, 'Jane', 'Smith', 28, 'Marketing', 55000.00);
 
-INSERT INTO Employees (EmployeeID, FirstName, LastName, Age, Department)
-VALUES (3, 'Michael', 'Johnson', 35, 'Finance');
+INSERT INTO employees (employee_id, first_name, last_name, age, department, salary)
+VALUES (3, 'Michael', 'Johnson', 35, 'Finance', 70000.00);
 
 -- Query to retrieve all employees from the Engineering department
-SELECT * FROM Employees WHERE Department = 'Engineering';
+SELECT * FROM employees WHERE department = 'Engineering';
+
+-- Query to calculate total employees and average salary by department
+SELECT department, COUNT(*) AS total_employees, AVG(salary) AS average_salary
+FROM employees
+GROUP BY department;
+
+-- Updating data: Increase salary for employees in the Engineering department by 10%
+UPDATE employees
+SET salary = salary * 1.10
+WHERE department = 'Engineering';
+
+-- Deleting data: Delete orders older than January 1, 2023
+DELETE FROM orders
+WHERE order_date < '2023-01-01';
